04aeba915333c807b433343f3871a66fb77b07f4
Valery Reddevotion <linkolnsid228@gmail.com>
2024-12-13 12:25:09 +0300

    Updates

--- assets/constants/constants.js
+++ assets/constants/constants.js
@@ -1,5 +1,5 @@
 const navLinks = [
-    {num: "00", text: "Home", link: "/"},
+    {num: "00", text: "Home", link: "/space-tourism-website/"},
     {num: "01", text: "Destination", link: "/destination"},
     {num: "02", text: "Crew", link: "/crew"},
     {num: "03", text: "Technology", link: "/technology"}
